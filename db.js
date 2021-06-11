be7db92397fdc776c6ece00bc915f884eab1638f
Viacheslav Zelinskyi <viacheslavzelinskyi@gmail.com>
2021-06-11 13:09:03 +0300

    Added SSL support for heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
-require('dotenv').config()
+require('dotenv').config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 // {
 // 	user: process.env.PG_USER,
 // 	host: process.env.PG_HOST,
@@ -13,7 +13,10 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 const proConfig = process.env.DATABASE_URL;
 
 const client = new Client({
- 	connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig
+	connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
-module.exports = client
+module.exports = client;
